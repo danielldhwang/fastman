a6ac54f92c01b8b09fbd901b3b8fe93d74e6f4b0
danioreo <danielldhwang@gmail.com>
2016-12-20 15:15:30 +1000

    fist commit

---
+++ R/testdata-data.R
@@ -0,0 +1,7 @@
+#' This is data to be included in my package
+#'
+#' @name testdata
+#' @docType data
+#' @author Daniel Liang-Dar Hwang \email{danielldhwang@@gmail.com}
+#' @keywords data
+"testdata"
